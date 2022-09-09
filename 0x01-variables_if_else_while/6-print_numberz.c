1bfbfe63f6c122736a812bbda985d2f2911f432f
Henree001 <okaforhenry779@gmail.com>
2022-09-09 15:07:54 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
