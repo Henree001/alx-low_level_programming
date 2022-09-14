87f7aea3e319658cb8b83871b879c3fce4d5b474
Henree001 <okaforhenry779@gmail.com>
2022-09-13 18:52:54 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - main function
+ *
+ * @n: integer to get absolute
+ *
+ * Return: int or int times negative
+ *
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	return (n * -1);
+}
--- 0x02-functions_nested_loops/put.c
+++ 0x02-functions_nested_loops/put.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	int print_sign(int n);
+	int _abs(int n);
 	return (0);
 }
