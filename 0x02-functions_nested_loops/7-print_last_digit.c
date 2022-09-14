662f7b13c0a9cc759f8322319900e9434f5eb891
Henree001 <okaforhenry779@gmail.com>
2022-09-13 19:23:53 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - main function
+ *
+ * @n: prints last digit
+ *
+ * Return: always n
+ *
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	_putchar('n');
+	return (n);
+}
--- 0x02-functions_nested_loops/put.c
+++ 0x02-functions_nested_loops/put.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	int _abs(int n);
+	int print_last_digit(int n);
 	return (0);
 }
