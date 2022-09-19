a2251d23952472d7ebb49bc1fe01e44ab12abbc5
Henree001 <okaforhenry779@gmail.com>
2022-09-19 11:43:04 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of two integers
+ *
+ * @a: first argument
+ * @b: second argumen
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	*b = *a;
+	*a = *b;
+}
