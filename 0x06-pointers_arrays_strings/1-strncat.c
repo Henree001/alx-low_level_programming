fce0a6a6c74e7a81fa8400865e3130d3803db800
Henree001 <okaforhenry779@gmail.com>
2022-09-21 19:49:30 +0000

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: destination of concat
+ * @src: source array to concat
+ * @n: number of characters to concat
+ *
+ * Return: char value
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len_dest;
+	int i;
+
+	for (len_dest = 0; dest[len_dest] != '\0'; len_dest++)
+	{}
+	for (i = 0; i < n; i++)
+		dest[len_dest + i] = src[i];
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char *_strcat(char *dest, char *src);
+	char *_strncat(char *dest, char *src, int n);
 	return (0);
 }
 
