7a443cb11eb2d9a2e794f947c4e67857d938f7e3
Henree001 <okaforhenry779@gmail.com>
2022-09-30 18:11:22 +0000

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+	if (argc < 1)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}	
+	printf("%d\n", sum);
+	return (0);
+}
+		
+
