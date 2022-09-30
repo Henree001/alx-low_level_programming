e1ff26110c89a7c6fcc15f10c921d660f808cac2
Henree001 <okaforhenry779@gmail.com>
2022-09-30 18:15:55 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 	if (argc < 1)
 	{
 		printf("0\n");
+		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
@@ -21,9 +22,8 @@ int main(int argc, char *argv[])
 			return (1);
 		}
 		sum += atoi(argv[i]);
-	}	
+	}
 	printf("%d\n", sum);
 	return (0);
-}
-		
+}	
 
