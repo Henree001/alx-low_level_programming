d9ffb7e285317c3cf76cbd48ae81894d08e73be3
Henree001 <okaforhenry779@gmail.com>
2022-10-03 17:12:08 +0000

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	if (size == 0)
+	if (size <= 0)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
 	if (array == NULL)
