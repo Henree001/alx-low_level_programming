d4379613f734197f6970850129a5fe8bf941cba6
Henree001 <okaforhenry779@gmail.com>
2022-10-06 17:23:46 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	new = malloc(sizeof(s1) + sizeof(s2));
+	new = malloc(strlen(s1) + sizeof(s2));
 	if (new == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
