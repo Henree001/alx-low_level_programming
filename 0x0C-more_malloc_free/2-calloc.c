611bc7d947bf01da1d3caa583c46987f61f4cf00
Henree001 <okaforhenry779@gmail.com>
2022-10-06 18:00:52 +0000

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - calloc function
+ * @nmemb: number of elements
+ * @size: size of bytes
+ * Return: pointer or void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *new;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	new = malloc(size * nmemb);
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < size * nmemb; i++)
+		new[i] = 0;
+	return (new);
+}
