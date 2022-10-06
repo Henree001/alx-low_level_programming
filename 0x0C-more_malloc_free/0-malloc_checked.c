384cf837845eddf9e830b492c7c4222b4087ec1b
Henree001 <okaforhenry779@gmail.com>
2022-10-06 16:38:03 +0000

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - checks malloc
+ * @b: integer to check?
+ * Return: a pointer or void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(sizeof(b));
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	void *malloc_checked(unsigned int b);
+	return (0);
+}
