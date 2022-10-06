4cb1cd7dc740211ece5ad7fbb25ff10e6bd2fda9
Henree001 <okaforhenry779@gmail.com>
2022-10-06 19:03:39 +0000

    task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - range of arrays
+ * @min: min of arrange
+ * @max: max of arrays
+ * Return: int value
+ */
+int *array_range(int min, int max)
+{
+	int i;
+	int *t;
+
+	if (min > max)
+		return (NULL);
+	for (i = min; i <= max; i++)
+	{}
+	t = malloc(sizeof(int) * i);
+	if (t == NULL)
+		return (NULL);
+	for (i = 0; i <= max; i++)
+	{
+		t[i] = min;
+		min++;
+	}
+	return (t);
+}
