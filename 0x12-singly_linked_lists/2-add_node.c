5f8def975a3715f2f27e645edd89a38a69623a62
Henree001 <okaforhenry779@gmail.com>
2022-10-20 21:37:09 +0000

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,8 +10,8 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *ptr = malloc(sizeof(list_t);
-	if (list_t ptr == NULL)
+	list_t *ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
 		return NULL;
 	ptr->str = strdup(str);
 	ptr->len = strlen(str);
