0317527b4a2fd054b34bedfc392df93a030047f8
Henree001 <okaforhenry779@gmail.com>
2022-10-24 17:18:43 +0000

    singly list task

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,17 +3,16 @@
 #include <string.h>
 #include "lists.h"
 /**
- * print_listint - prints all the elements of a listint_t list.
+ * listint_len - returns the number of elements of a listint_t list.
  * @h: pointer to head
  * Return: number of nodes.
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	unsigned long int i = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
