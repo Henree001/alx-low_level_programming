570cf9e61988de1806fc15985afc04db6d9d9936
Henree001 <okaforhenry779@gmail.com>
2022-10-29 00:06:04 +0000

    tasks

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,9 @@
 #include "2-get_bit.c"
 /**
  * set_bit - sets a bit
- * @n: integer to grab 
+ * @n: integer to grab
+ * @index: index number
+ * Return: 1 if true or -1 if otherwise
  *
  */
 int set_bit(unsigned long int *n, unsigned int index)
