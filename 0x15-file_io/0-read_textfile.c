a8058675e72d8bbc587f0607bddd80719faf5a25
Henree001 <okaforhenry779@gmail.com>
2022-11-02 08:18:03 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDWR);
-	if (fd == NULL)
+	if (fd == -1)
 		return (0);
 	rd = read(fd, buff, letters);
 	if (rd == -1)
