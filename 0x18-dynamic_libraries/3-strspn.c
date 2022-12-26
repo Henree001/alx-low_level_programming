d74178b8c7ec7869cdc81ad587063eb79679d365
Henree001 <okaforhenry779@gmail.com>
2022-12-26 21:14:58 +0000

    tasks

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strspn - length of prefix substring
+ *
+ * @s: string to go through
+ * @accept: accepted bytes
+ *
+ * Return: returns usigned values
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0;
+
+	if ((*s == '\0') || (*accept == '\0'))
+		return (len);
+	while (*s && _strchr(accept, *s++))
+	{
+		len++;
+	}
+	return (len);
+}
