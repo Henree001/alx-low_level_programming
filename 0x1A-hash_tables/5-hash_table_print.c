1026c7488425ee8b7c00b2782932d49fb828f07a
Henree001 <okaforhenry779@gmail.com>
2023-01-24 21:09:11 +0000

    tasks

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,22 +9,21 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *item;
-	char *c = ", ";
 
 	if (ht == NULL)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-
 		item = ht->array[i];
-		if (i == ht->size - 1)
-			*c = "}";
 		while (item)
 		{
 			printf("'%s': '%s'%s", item->key, item->value, c);
+			if (ht->array[i + 1] == NULL)
+				break;
+			printf(", ");
 			item = item->next;
 		}
 	}
-	printf("\n");
+	printf("}\n");
 }
